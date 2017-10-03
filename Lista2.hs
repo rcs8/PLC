ec4c0c45b888909d0e828b7459ed7e5397303e47
rcs8 <rcs8@cin.ufpe.br>
2017-10-03 12:18:16 -0300

    Update Lista2.hs

--- Lista2.hs
+++ Lista2.hs
@@ -10,3 +10,30 @@ testaListaM f l = and(map f l)
 
 testaListaF :: (a -> Bool) -> [a] -> Bool
 testaListaF f l = foldr (\a b -> f a && b) True l
+
+type Nome = String
+type Conteudo = String
+<<<<<<< HEAD
+data Arquivo = Simples Nome Conteudo | Diretorio Nome [Arquivo] deriving (Eq)
+
+instance Show Arquivo where
+  show (Simples n _) = n
+  show (Diretorio n _) = n
+
+nomeArquivo :: Arquivo -> Nome
+nomeArquivo (Simples n _) = n
+nomeArquivo (Diretorio n _) = n
+
+mudarDir :: Arquivo -> Nome -> Nome
+mudarDir (Simples n c) name = "Não existe"
+mudarDir (Diretorio n a) name
+ |n==nom = n
+ |nom 'elem' [mudarDir n name] | x <- a] = nomeArquivo
+ |otherwise = "Nao Existe"
+
+allArq :: Arquivo -> [Nome]
+allArq (Simples n c) = [n]
+allArq (Diretorio n []) = []
+allArq (Diretorio n (x:xs)) = allArq x ++ allArq (Diretorio n xs)
+=======
+>>>>>>> 2209f26a6b3d8fd95360c49b93aac2339d36dc0e
